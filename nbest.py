95afd01b49fcec53c3d0adbe3a28ad495e196b53
markvanheeswijk <mark.van.heeswijk@gmail.com>
2016-12-23 20:10:52 +0100

    initial commit

---
+++ nbest.py
@@ -0,0 +1,16 @@
+# keep a list of the N best things we have seen, discard anything else
+class nbest(object):
+    def __init__(self,N=1000):
+        self.store = []
+        self.N = N
+        
+    def add(self,item):
+        self.store.append(item)
+        self.store.sort(reverse=True)
+        self.store = self.store[:self.N]
+    
+    def __getitem__(self,k):
+        return self.store[k]
+
+    def __len__(self):
+        return len(self.store)
